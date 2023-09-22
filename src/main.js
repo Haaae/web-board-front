5bcb0b292829e711f7a09a2e1065d1df174c97b8
Haaae <101785628+Haaae@users.noreply.github.com>
2023-09-22 14:30:46 +0900

    feat: vue 적용사항에 따른 수정

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+
 import router from './router'
+
 import '/plugin/vee-validation.js'
 
-import { store } from './store'
+import { store } from './store/index'
 
 import axios from 'axios'
 
-Vue.use(axios)
-// Vue.prototype.$axios = window.axios
+// Vue.use(axios)
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 

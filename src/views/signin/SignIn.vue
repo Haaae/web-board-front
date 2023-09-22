40b0019cb3334dc1b35e8ed3345d7e085f2bb25a
Haaae <101785628+Haaae@users.noreply.github.com>
2023-09-22 14:26:39 +0900

    feat: 로그인 페이지 구현

--- src/views/signin/SignIn.vue
+++ src/views/signin/SignIn.vue
@@ -9,13 +9,31 @@
 		<v-row justify="center">
 			<v-col align-self="center" lg="5" md="6" sm="8" xs="9">
 
+				<validation-observer ref="validator" v-slot="{ invalid }">
 				<v-card>
 					<v-card-title>
 						<span class="text-h5">Login</span>
 					</v-card-title>
 					<v-card-text>
+
 						<v-form>
-							<v-text-field v-model="username" label="Username" prepend-icon="mdi-account-circle" />
+							<validation-provider 
+							v-slot="{ errors }"
+							name="제목"
+							:rules="{ required: true, email: true }">
+							
+								<v-text-field 
+								v-model="username" 
+								label="Username" 
+								prepend-icon="mdi-account-circle" 
+								:error-messages="errors"
+								/>
+							</validation-provider>
+
+							<validation-provider 
+							v-slot="{ errors }"
+							name="제목"
+							:rules="{ required: true, password: true }">
 							<v-text-field
 								v-model="password"
 								:type="showPassword ? 'text' : 'password'"
@@ -23,14 +41,16 @@
 								prepend-icon="mdi-lock"
 								:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
 								@click:append="showPassword = !showPassword"
+								:error-messages="errors"
 							/>
+							</validation-provider>
 						</v-form>
 					</v-card-text>
 
 					<v-card-actions>
 						<v-btn color="success" to="/signup" dark>Register</v-btn>
-						<v-spacer></v-spacer>
-						<v-btn @click="login" color="info">Login</v-btn>
+						<v-spacer/>
+						<v-btn @click="doLogin" :disabled="invalid" color="info">Login</v-btn>
 					</v-card-actions>
 					<v-card-actions>
 						<v-spacer />
@@ -43,6 +63,8 @@
 						</v-btn>
 					</v-card-actions>
 				</v-card>
+				</validation-observer>
+
 			</v-col>
 
 		</v-row>
@@ -50,6 +72,10 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
+import '/plugin/vee-validation.js'
+
+const userStore = 'userStore'
 
 export default {
 	name: 'LoginPage',
@@ -61,17 +87,32 @@ export default {
 			dialog: true,
 		}
 	},
+	created() {
+        if (this.isLoginUser) {
+            this.$router.push({name: 'Main'})
+        }
+    },
 	methods: {
+		...mapActions(userStore, ['login']),
         close() {
 			this.$router.go(-1)
         },
-		login() {
-			this.$store.dispatch(
-				"login", 
-				{username: this.username, password : this.password}
-			)
+		doLogin() {
+			this.login({
+				username: this.username,
+				password : this.password
+			})
+			
+			if (this.isLoginUser) {
+				close()
+			}
 		}
-		
     },
+	computed: {
+		...mapGetters(userStore, [
+			'sessionId', 
+			'isLoginUser'
+		])
+	}
 }
 </script>

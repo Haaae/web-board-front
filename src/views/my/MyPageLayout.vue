4f84cf5b6a22e7d6300ecac6287e8652632d9500
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-03 11:24:52 +0900

    feat: mypage layout에 링크 가이드 ui 구현

--- src/views/my/MyPageLayout.vue
+++ src/views/my/MyPageLayout.vue
@@ -1,8 +1,32 @@
 <template>
+  <v-container fluid>
     <div>
-        <h1>MyPageLayout</h1>
-        <router-view></router-view>
+        <v-row
+          align="start"
+        >
+          <v-col class="pb-0 pl-0">
+            <v-btn link to="/" text class="px-0" >
+              home
+            </v-btn>
+            
+            <v-icon>mdi-chevron-right</v-icon>
+            
+            <v-btn
+            class="pl-2"
+            text
+            @click="()=>this.$router.push({name:'MyPageHome'})"
+            >
+            mypage
+            </v-btn>
+
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <router-view />
+        </v-row>
     </div>
+  </v-container>
   </template>
   
   <script>

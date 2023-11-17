15424ba9eabf4fbabce2838c66d580aeacb665fb
김민규 <alsrbtls88@gmail.com>
2023-11-17 18:35:23 +0900

    feat: home welcome 카드 ui 구현

---
+++ src/components/utils/cardLinkBtn.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-btn
+    outlined
+    rounded
+    link
+    color="primary"
+    :href="link"
+    >   
+        {{ text }}
+    </v-btn>
+</template>
+
+<script>
+export default {
+    name: "CardLinkBtn",
+    props: {
+        link: {type: String, required: true},
+        text: {type: String, required: true},
+    },
+}
+
+</script>
--- src/views/main/MainPageLayout.vue
+++ src/views/main/MainPageLayout.vue
@@ -1,29 +1,81 @@
 <template>
   <v-container fluid class="pa-4 pa-sm-6 pa-md-8">
-    <h1 class="pa-4">MainPageLayout</h1>
-    <v-row class="grey">
+    <v-row justify="center">
       <v-col>
-        <v-btn
-        to="/signup"
-        color="primary"
-        dark
+
+        <v-row>
+          <div
+          style="height: 250px;"
+          />
+        </v-row>
+
+        <v-card
+          class="mx-auto"
+          max-width="500"
+          outlined
         >
-        SIGN UP
-      </v-btn>
-    </v-col>  
-  </v-row>
+
+          <v-list-item three-line>
+            <v-list-item-content>
+              <div class="text-overline mb-4">
+                Welcome
+              </div>
+              <v-list-item-title class="text-h5 mb-1">
+                게시판 프로젝트
+              </v-list-item-title>
+
+              <v-list-item-subtitle class="mt-5">
+                Vue.js, Spring으로 구성된 웹 프로젝트입니다.
+              </v-list-item-subtitle>
+              <v-list-item-subtitle class="mt-2">
+                더 많은 정보를 보려면 하단의 버튼를 클릭하세요!
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-card-actions class="mb-2 ml-3">
+            <cardLinkBtn
+            class="mr-2"
+            :link="portfolioLink"
+            :text="'portfolio'"
+            />
+            
+            <cardLinkBtn
+            class="mr-2"
+            :link="backendLink"
+            :text="'server-github'"
+            />
+            
+            <cardLinkBtn
+            class="mr-2"
+              :link="frontendLink"
+              :text="'view-Github'"
+            />
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   
-  <router-view />
-  
-  </v-container>
+    </v-container>
   </template>
   
-  <script>
-  export default {
-    name: "MainPageLayout",
-  }
-  </script>
+<script>
+import cardLinkBtn from '@/components/utils/cardLinkBtn.vue'
+
+export default {
+  name: "MainPageLayout",
   
-  <style scoped>
+  components: {
+    cardLinkBtn
+  },
   
-  </style>
+  data() {
+    return {
+      frontendLink: 'https://github.com/Haaae/web-board-front',
+      backendLink: 'https://github.com/Haaae/web-board',
+      portfolioLink: 'https://furtive-stew-f33.notion.site/My-Board-90459e8c070b456bae542327dc9a6c39',
+    }
+  },
+}
+</script>
+  
